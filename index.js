2b207dd85c493e1b9c821f500ef049e7d81f2d99
Andrew Fryer <andrew.fryer@queensu.ca>
2020-06-04 21:32:28 +0100

    this works beautifully on chromium, but is not supported when I test on Firefox

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-let el = document.getElementById("my-form");
-el.onsubmit = e => {
+let form = document.getElementById("my-form");
+let usernameInput = document.getElementById("my-username");
+let passwordInput = document.getElementById("my-password");
+
+const isCorrect = password => password.length === 4;
+
+form.onsubmit = e => {
   e.preventDefault();
   console.log("form submitted");
+
+  // let cred = {
+  //   id: usernameInput.value,
+  //   password: passwordInput.value,
+  // };
+  if(isCorrect(passwordInput.value)) {
+    let cred = new PasswordCredential({
+      id: usernameInput.value,
+      password: "testing", //"passwordInput.value",
+    });
+    window.navigator.credentials.store(cred);
+  }
 };
 
-const breakFn = (e, s) => {
-  console.log(s); // put breakpoint here
-}
-for(let elem of document.querySelectorAll('*')) {
-  const event = "keypress";
-  elem.addEventListener(event, e => breakFn(e, `Capturing: ${elem.tagName}`), true);
-  elem.addEventListener(event, e => breakFn(e, `Bubbling: ${elem.tagName}`));
-}
+console.log(window.navigator.credentials);
+
+window.navigator.credentials.get({password: true}).then(c => {
+  console.log(c);
+});
