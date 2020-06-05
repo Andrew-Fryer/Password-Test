d4498cfea439b71200311e85d614b9130273812e
Andrew Fryer <andrewf@kingsds.network>
2020-06-05 09:55:51 -0400

    don't log so much and preventdefault

--- index.js
+++ index.js
@@ -14,18 +14,10 @@ passwordInput.onchange = () => {
 passwordInput.addEventListener("invalid", e => {
   //form.reset();
   passwordInput.value = "";
+  e.preventDefault();
 });
 
 form.onsubmit = e => {
   e.preventDefault();
   console.log("form submitted");
 };
-
-const breakFn = s => {
-  console.log(s); // put a breakpoint here
-}
-for(let elem of document.querySelectorAll('*')) {
-  
-  elem.addEventListener("submit", e => breakFn(`Capturing: ${elem.tagName}`), true);
-  elem.addEventListener("submit", e => breakFn(`Bubbling: ${elem.tagName}`));
-}
