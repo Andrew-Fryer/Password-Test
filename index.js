307e8a4c92ea09536cbd2790ca409f70cf7dc3c4
Andrew Fryer <andrewf@kingsds.network>
2020-06-05 14:20:55 -0400

    did some testing and it looks like this is a viable solution for Chrome, Firfox, Chromium, and also works for lastpass

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let createForm = document.getElementById("my-form-create");
+let createPasswordInput = document.getElementById("my-password-create");
+let createPasswordInputConfirm = document.getElementById("my-password-create-confirm");
+
+const setValidity = () =>
+  createPasswordInput.value === createPasswordInputConfirm.value
+    ? createPasswordInputConfirm.setCustomValidity("")
+    : createPasswordInputConfirm.setCustomValidity("Passwords must match.");
+
+createPasswordInput.onchange = setValidity;
+createPasswordInputConfirm.onchange = setValidity;
+
+createPasswordInputConfirm.addEventListener("invalid", e => {
+  e.preventDefault();
+  alert("please make the passwords match");
+});
+
+createForm.onsubmit = e => {
+  e.preventDefault();
+  console.log("create form submitted");
+
+  setTimeout(function(){
+    createForm.hidden = true; // this happens for the password modal anyway
+      history.replaceState(null, document.title); // this should signal to the browser that the login was successful
+  }, 1);
+}
+
 let passwordInput = document.getElementById("my-password");
 let form = document.getElementById("my-form");
 
@@ -22,8 +49,7 @@ form.onsubmit = e => {
   console.log("form submitted");
 
   setTimeout(function(){
-    form.hidden = true; // this happens for the password modal anyway
-    history.replaceState(null, document.title); // this should signal to the browser that the login was successful
-}, 1);
+      form.hidden = true; // this happens for the password modal anyway
+      history.replaceState(null, document.title); // this should signal to the browser that the login was successful
+  }, 1);
 };
-
